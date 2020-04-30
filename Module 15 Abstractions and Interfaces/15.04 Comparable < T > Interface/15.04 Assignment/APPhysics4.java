c9bd8528e310959611fc5d6142bc1b399a91ad36
peetacho <64259077+peetacho@users.noreply.github.com>
2020-04-30 21:30:38 +0800

    Add files via upload

---
+++ Module 15 Abstractions and Interfaces/15.04 Comparable < T > Interface/15.04 Assignment/APPhysics4.java
@@ -0,0 +1,31 @@
+/*
+
+
+*/
+
+public class APPhysics4 extends Homework4{
+
+	private int pagesRead;
+	private String hwType;
+
+
+	public APPhysics4(){
+		super();
+		
+	}
+
+	public void createAssignment(int pr){
+		setNumPages(pr);
+		hwType = "AP Physics";
+	}
+
+	public String toString(){
+		return hwType + " - read " + getNumPages() + " pages.";
+	}
+
+	public String getType(){
+		return hwType;
+	}
+
+
+}
